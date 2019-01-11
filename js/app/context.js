8433db560e588adcf0fafb7e977794f79ba42786
ya <ya@dei.su>
2019-01-11 15:05:38 +0300

    Added app.context

---
+++ js/app/context.js
@@ -0,0 +1,50 @@
+var ApplicationContext = (function(){
+	var account = {};
+	var assets = {};
+	function getAssetsList() {
+		return Object.keys(assets).map(function (key) {
+			return assets[key];
+		});
+	}
+	function putAsset(issueTransaction) {
+		console.log('putAsset', issueTransaction.name);
+		var currency = Currency.create({
+			id: issueTransaction.assetId,
+			displayName: issueTransaction.name,
+			precision: issueTransaction.decimals
+		});
+		var asset = {
+			currency: currency,
+			description: issueTransaction.description,
+			timestamp: issueTransaction.timestamp,
+			sender: issueTransaction.sender,
+			totalTokens: Money.fromCoins(issueTransaction.quantity, currency)
+		};
+		var balance;
+		if (angular.isDefined(assets[currency.id])) {
+			balance = assets[currency.id].balance;
+		} else {
+			balance = new Money(0, currency);
+		}
+		asset.balance = balance;
+		assets[currency.id] = asset;
+	}
+	function updateAsset(assetId, balance, reissuable, totalTokens) {
+		console.log('updateAsset()', assetId, balance, reissuable, totalTokens);
+		var asset = assets[assetId];
+		if (asset) {
+			asset.balance = Money.fromCoins(balance, asset.currency);
+			asset.totalTokens = Money.fromCoins(totalTokens, asset.currency);
+			asset.reissuable = reissuable;
+		}
+	}
+	return {
+		account: account,
+		cache: {
+			assets: assets,
+			getAssetsList: getAssetsList,
+			putAsset: putAsset,
+			updateAsset: updateAsset
+		}
+	}
+})();
