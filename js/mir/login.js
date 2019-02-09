60a0a0849a441fc46dea360c82652628ab140fe9
ya <ya@dei.su>
2019-02-09 16:46:55 +0300

    Upd

--- js/mir/login.js
+++ js/mir/login.js
@@ -1,19 +1,3 @@
-/******************************************************************************
- * Copyright © 2016 The Waves Developers.                                     *
- *                                                                            *
- * See the LICENSE files at                                                   *
- * the top-level directory of this distribution for the individual copyright  *
- * holder information and the developer policies on copyright and licensing.  *
- *                                                                            *
- * Unless otherwise agreed in a custom licensing agreement, no part of the    *
- * Waves software, including this file, may be copied, modified, propagated,  *
- * or distributed except according to the terms contained in the LICENSE      *
- * file.                                                                      *
- *                                                                            *
- * Removal or modification of this copyright notice is prohibited.            *
- *                                                                            *
- ******************************************************************************/
-
 (function() {
 	'use strict';
 
@@ -439,11 +423,18 @@
 		.controller('accountLoginController', AccountLoginController);
 })();
 
-(function() {
-	'use strict';
-
-	angular.module('app.navigation', ['waves.core.services', 'app.ui', 'app.shared'])
-		.constant('navigation.events', {
-			NAVIGATION_CREATE_ALIAS: 'navigation-create-alias'
-		});
-})();
+/******************************************************************************
+ * Copyright © 2016 The Waves Developers.                                     *
+ *                                                                            *
+ * See the LICENSE files at                                                   *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * Waves software, including this file, may be copied, modified, propagated,  *
+ * or distributed except according to the terms contained in the LICENSE      *
+ * file.                                                                      *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
